ac042b8ab1c6ebe34312c7309863fca1d8c8fd80
Chris Bradford <87334834+bradford583@users.noreply.github.com>
2021-10-18 20:47:28 -0600

    about to deploy to heroku

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
 // get all Users
 router.get('/', (req, res) => {
-    console.log('==================');
     Post.findAll({
-        //query configuration
+        order: [['created_at', 'DESC']],
         attributes: [
             'id',
             'post_url',
@@ -14,8 +13,15 @@ router.get('/', (req, res) => {
             'created_at',
             [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
             ],
-        order: [['created_at', 'DESC']],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
